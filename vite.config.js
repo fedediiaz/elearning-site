2022a423d737aafaaa510e8761ffac1a8b94f4e2
Federico Diaz <federico.n.diaz92@gmail.com>
2023-12-20 18:44:38 +0100

    remove svg loader

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,10 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-import svgLoader from "vite-svg-loader";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    svgLoader({
-      // Optional options
-      process: true, // Enable automatic SVG processing
-      source: './src/assets', // Specify the source directory for SVG files
-      inline: true, // Inline SVGs into the HTML instead of creating components
-      optimize: true, // Optimize SVGs for better performance
-    }),
   ],
   resolve: {
     alias: {
