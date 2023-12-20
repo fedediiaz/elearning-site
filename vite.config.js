7a7d578069eb31d83f5266a3df15749eaa3da50b
Federico Diaz <federico.n.diaz92@gmail.com>
2023-12-20 18:30:47 +0100

    Possible fix for images 2

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,16 @@ import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue(),
+    svgLoader({
+      // Optional options
+      process: true, // Enable automatic SVG processing
+      source: './src/assets', // Specify the source directory for SVG files
+      inline: true, // Inline SVGs into the HTML instead of creating components
+      optimize: true, // Optimize SVGs for better performance
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
